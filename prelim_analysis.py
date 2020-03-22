e083848235c3fcf8e7dcfe5fab61137fb6a01e7b
itskeith <itskeithryan@gmail.com>
2020-03-22 18:42:17 +0000

    added basic write to text file based on print statements describing the dataset

--- prelim_analysis.py
+++ prelim_analysis.py
@@ -6,11 +6,29 @@ import matplotlib.pyplot as plt
 #iris_df is the dataframe storing the full iris dataset
 iris_df = pd.read_csv('iris-flower-dataset\IRIS.csv')
 
-#print information about the dataset to the command line
-print("\nThis is a summary of all the numeric columns of the Iris Flower Dataset:")
-print(iris_df.describe())
+#after doing a bit of digging we can use .to_string on the describe pandas function to give us a str representation of our dataframe object
+iris_df_description = iris_df.describe().to_string() #took inspiration from stackOverflow: https://stackoverflow.com/questions/51829923/write-a-pandas-dataframe-to-a-txt-file
 
-print("\n This is the top 5 rows of the dataset, this shows us one of the flower species and all of the column names:")
+#open summary.txt file or create if doesn't exist and write to it
+with open('Preliminary Analysis Output\summary.txt', 'w') as summaryFile:
+    #start writing to file, summary info from dataframe summary functions like describe(), head(), unique() etc.
+    summaryFile.write('This is a summary of all the numeric columns of the Iris Flower Dataset:\n\n') #double new line for readability
+    summaryFile.write(f"{iris_df_description} \n\n")
+    summaryFile.write('This is the top 5 rows of the dataset, this shows us one of the flower species and all of the column names:\n\n')
+    summaryFile.write(f"{iris_df.head().to_string()} \n\n") #write's head or top 5 rows of dataframe
+    summaryFile.write("This is the tail: \n")
+    summaryFile.write(f"{iris_df.tail().to_string()} \n\n") #write tail/bottom 5 rows of dataframe
+    summaryFile.write(f"The total number of rows is {len(iris_df)}\n\n")
+    uniqueSpecies = iris_df['species'].unique() #store all the unique species in a list
+    for species in uniqueSpecies:
+        summaryFile.write(f"{species}\n") #write each item of list to text file on new line
+    summaryFile.write("\nPlease find all generated .png's of plots in this same folder")
+
+#leaving in old print statements for now, so still getting messages in console
+print("\nThis is a summary of all the numeric columns of the Iris Flower Dataset:")
+print(iris_df_description)
+
+print("This is the top 5 rows of the dataset, this shows us one of the flower species and all of the column names:")
 print(iris_df.head())
 
 print("\n This is the tail: ")
