1db6fd612c8eb2a64d1d06dfedd98cdc56549e9b
itskeith <itskeithryan@gmail.com>
2020-03-22 20:45:09 +0000

    add transparency to histograms when overlayed

--- prelim_analysis_hist.py
+++ prelim_analysis_hist.py
@@ -12,174 +12,147 @@ def hist_subplot_iris(iris_df):
     #first row show all species
     axes[0][0].hist(iris_df['petal_length'])#using row,col indexes on ax
     axes[0][0].set_title('All species petal length')
-    axes[0][0].legend(loc='upper right')
     axes[0][0].set_ylabel('No. Petals')
     axes[0][0].set_xlabel('Petal Length')
 
     #histogram for petal_width, no filter
     axes[0][1].hist(iris_df['petal_width'])
     axes[0][1].set_title('All species petal width')
-    axes[0][1].legend(loc='upper right')
     axes[0][1].set_ylabel('No. Petals')
     axes[0][1].set_xlabel('Petal Width')
 
     #histogram for sepal_length, no filter
     axes[0][2].hist(iris_df['sepal_length'])
     axes[0][2].set_title('All species sepal length')
-    axes[0][2].legend(loc='upper right')
     axes[0][2].set_ylabel('No. Petals')
     axes[0][2].set_xlabel('Sepal Length')
 
     #histogram for sepal_width, no filter
     axes[0][3].hist(iris_df['sepal_width'])
     axes[0][3].set_title('All species sepal width')
-    axes[0][3].legend(loc='upper right')
     axes[0][3].set_ylabel('No. Petals')
     axes[0][3].set_xlabel('Sepal Width')
 
     #For the next row of my set of subplots, I'm going to show a overlayed histogram
-    axes[1][0].hist(iris_setosa_df['petal_length'], color='red')
+    axes[1][0].hist(iris_setosa_df['petal_length'], color='red', alpha=0.5) #alpha sets the amount of transparency for the histograms
     axes[1][0].set_title('Iris-Setosa petal length')
-    axes[1][0].legend(loc='upper right')
     axes[1][0].set_ylabel('No. Petals')
     axes[1][0].set_xlabel('Petal Length')
 
-    axes[1][1].hist(iris_setosa_df['petal_width'], color='red')
+    axes[1][1].hist(iris_setosa_df['petal_width'], color='red', alpha=0.5)
     axes[1][1].set_title('Iris-Setosa petal width')
-    axes[1][1].legend(loc='upper right')
     axes[1][1].set_ylabel('No. Petals')
     axes[1][1].set_xlabel('Petal Width')
 
-    axes[1][2].hist(iris_setosa_df['sepal_length'], color='red')
+    axes[1][2].hist(iris_setosa_df['sepal_length'], color='red', alpha=0.5)
     axes[1][2].set_title('Iris-Setosa sepal length')
-    axes[1][2].legend(loc='upper right')
     axes[1][2].set_ylabel('No. Petals')
     axes[1][2].set_xlabel('Sepal Length')
 
-    axes[1][3].hist(iris_setosa_df['sepal_width'], color='red')
+    axes[1][3].hist(iris_setosa_df['sepal_width'], color='red', alpha=0.5)
     axes[1][3].set_title('Iris-Setosa sepal width')
-    axes[1][3].legend(loc='upper right')
     axes[1][3].set_ylabel('No. Petals')
     axes[1][3].set_xlabel('Sepal Width')
-    axes[1][0].hist(iris_virginica_df['petal_length'], color='orange')
+
+    axes[1][0].hist(iris_virginica_df['petal_length'], color='orange', alpha=0.5)
     axes[1][0].set_title('Iris-Virginica petal length')
-    axes[1][0].legend(loc='upper right')
     axes[1][0].set_ylabel('No. Petals')
     axes[1][0].set_xlabel('Petal Length')
 
-    axes[1][1].hist(iris_virginica_df['petal_width'], color='orange')
+    axes[1][1].hist(iris_virginica_df['petal_width'], color='orange', alpha=0.5)
     axes[1][1].set_title('Iris-Virginica petal width')
-    axes[1][1].legend(loc='upper right')
     axes[1][1].set_ylabel('No. Petals')
     axes[1][1].set_xlabel('Petal Width')
 
-    axes[1][2].hist(iris_virginica_df['sepal_length'], color='orange')
+    axes[1][2].hist(iris_virginica_df['sepal_length'], color='orange', alpha=0.5)
     axes[1][2].set_title('Iris-Virginica sepal length')
-    axes[1][2].legend(loc='upper right')
     axes[1][2].set_ylabel('No. Petals')
     axes[1][2].set_xlabel('Sepal Length')
 
-    axes[1][3].hist(iris_virginica_df['sepal_width'], color='orange')
+    axes[1][3].hist(iris_virginica_df['sepal_width'], color='orange', alpha=0.5)
     axes[1][3].set_title('Iris-Virginica sepal width')
-    axes[1][3].legend(loc='upper right')
     axes[1][3].set_ylabel('No. Petals')
     axes[1][3].set_xlabel('Sepal Width')
 
-    axes[1][0].hist(iris_versicolor_df['petal_length'], color='green')
+    axes[1][0].hist(iris_versicolor_df['petal_length'], color='green', alpha=0.5)
     axes[1][0].set_title('Iris-Versicolor petal length')
-    axes[1][0].legend(loc='upper right')
     axes[1][0].set_ylabel('No. Petals')
     axes[1][0].set_xlabel('Petal Length')
 
-    axes[1][1].hist(iris_versicolor_df['petal_width'], color='green')
+    axes[1][1].hist(iris_versicolor_df['petal_width'], color='green', alpha=0.5)
     axes[1][1].set_title('Iris-Versicolor petal width')
-    axes[1][1].legend(loc='upper right')
     axes[1][1].set_ylabel('No. Petals')
     axes[1][1].set_xlabel('Petal Width')
 
-    axes[1][2].hist(iris_versicolor_df['sepal_length'], color='green')
+    axes[1][2].hist(iris_versicolor_df['sepal_length'], color='green', alpha=0.5)
     axes[1][2].set_title('Iris-Versicolor sepal length')
-    axes[1][2].legend(loc='upper right')
     axes[1][2].set_ylabel('No. Petals')
     axes[1][2].set_xlabel('Sepal Length')
 
-    axes[1][3].hist(iris_versicolor_df['sepal_width'], color='green')
+    axes[1][3].hist(iris_versicolor_df['sepal_width'], color='green', alpha=0.5)
     axes[1][3].set_title('Iris-Versicolor sepal width')
-    axes[1][3].legend(loc='upper right')
     axes[1][3].set_ylabel('No. Petals')
     axes[1][3].set_xlabel('Sepal Width')
 
     #Here we start to add our different species in isolation and add our Iris_setosa_df row to the subplot
     axes[2][0].hist(iris_setosa_df['petal_length'], color='red')
     axes[2][0].set_title('Iris-Setosa petal length')
-    axes[2][0].legend(loc='upper right')
     axes[2][0].set_ylabel('No. Petals')
     axes[2][0].set_xlabel('Petal Length')
 
     axes[2][1].hist(iris_setosa_df['petal_width'], color='red')
     axes[2][1].set_title('Iris-Setosa petal width')
-    axes[2][1].legend(loc='upper right')
     axes[2][1].set_ylabel('No. Petals')
     axes[2][1].set_xlabel('Petal Width')
 
     axes[2][2].hist(iris_setosa_df['sepal_length'], color='red')
     axes[2][2].set_title('Iris-Setosa sepal length')
-    axes[2][2].legend(loc='upper right')
     axes[2][2].set_ylabel('No. Petals')
     axes[2][2].set_xlabel('Sepal Length')
 
     axes[2][3].hist(iris_setosa_df['sepal_width'], color='red')
     axes[2][3].set_title('Iris-Setosa sepal width')
-    axes[2][3].legend(loc='upper right')
     axes[2][3].set_ylabel('No. Petals')
     axes[2][3].set_xlabel('Sepal Width')
 
     axes[3][0].hist(iris_virginica_df['petal_length'], color='orange')
     axes[3][0].set_title('Iris-Virginica petal length')
-    axes[3][0].legend(loc='upper right')
     axes[3][0].set_ylabel('No. Petals')
     axes[3][0].set_xlabel('Petal Length')
 
     axes[3][1].hist(iris_virginica_df['petal_width'], color='orange')
     axes[3][1].set_title('Iris-Virginica petal width')
-    axes[3][1].legend(loc='upper right')
     axes[3][1].set_ylabel('No. Petals')
     axes[3][1].set_xlabel('Petal Width')
 
     axes[3][2].hist(iris_virginica_df['sepal_length'], color='orange')
     axes[3][2].set_title('Iris-Virginica sepal length')
-    axes[3][2].legend(loc='upper right')
     axes[3][2].set_ylabel('No. Petals')
     axes[3][2].set_xlabel('Sepal Length')
 
     axes[3][3].hist(iris_virginica_df['sepal_width'], color='orange')
     axes[3][3].set_title('Iris-Virginica sepal width')
-    axes[3][3].legend(loc='upper right')
     axes[3][3].set_ylabel('No. Petals')
     axes[3][3].set_xlabel('Sepal Width')
 
     #Finally we add our Iris_verisoclor_df row to the subplot
     axes[4][0].hist(iris_versicolor_df['petal_length'], color='green')
     axes[4][0].set_title('Iris-Versicolor petal length')
-    axes[4][0].legend(loc='upper right')
     axes[4][0].set_ylabel('No. Petals')
     axes[4][0].set_xlabel('Petal Length')
 
     axes[4][1].hist(iris_versicolor_df['petal_width'], color='green')
     axes[4][1].set_title('Iris-Versicolor petal width')
-    axes[4][1].legend(loc='upper right')
     axes[4][1].set_ylabel('No. Petals')
     axes[4][1].set_xlabel('Petal Width')
 
     axes[4][2].hist(iris_versicolor_df['sepal_length'], color='green')
     axes[4][2].set_title('Iris-Versicolor sepal length')
-    axes[4][2].legend(loc='upper right')
     axes[4][2].set_ylabel('No. Petals')
     axes[4][2].set_xlabel('Sepal Length')
 
     axes[4][3].hist(iris_versicolor_df['sepal_width'], color='green')
     axes[4][3].set_title('Iris-Versicolor sepal width')
-    axes[4][3].legend(loc='upper right')
     axes[4][3].set_ylabel('No. Petals')
     axes[4][3].set_xlabel('Sepal Width')
 
